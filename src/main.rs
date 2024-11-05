31515ee3e76ef620e7893ef390e8355ca0e66ebc
lynx <76166621+LynX-gh@users.noreply.github.com>
2024-11-05 16:13:39 +0530

    Parse Correlation ID
    Signed-off-by: lynx <76166621+LynX-gh@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused_imports)]
-use std::io::Write;
+use std::io::{Cursor, Read, Write};
 use std::net::TcpListener;
 use bytes::BufMut;
 
@@ -14,14 +14,39 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let message_size:i32 = 0;
-                let correlation_id:i32 = 7;
-
                 let mut response = vec![];
-                response.put_i32(message_size);
-                response.put_i32(correlation_id);
+                let mut len_buf = [0_u8; 4];
+                match _stream.read_exact(&mut len_buf) {
+                    Ok(_len) => {
+                        println!("Incoming Message Length : {:?}", len_buf);
+                        let msg_len = i32::from_be_bytes(len_buf);
 
-                _stream.write_all(&response).unwrap();
+                        let mut msg_buf = vec![0_u8; msg_len as usize];
+                        match _stream.read_exact(&mut msg_buf) {
+                            Ok(_len) => {
+                                println!("Incoming Message Buffer : {:?}", msg_buf);
+
+                                let message_size:i32 = 20;
+
+                                response.put_i32(message_size);
+                                response.extend(&msg_buf[4..8]);
+                                response.put_i16(35_i16);
+                            }
+                            Err(e) => {
+                                println!("error: {}", e);
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        println!("error: {}", e);
+                    }
+                }
+
+                // println!("RESP : {:?}", response);
+
+                _stream.write(&response).unwrap();
+                let mut _remaining_buf = vec![];
+                _stream.read_to_end(&mut _remaining_buf).unwrap();
 
                 println!("accepted new connection");
             }
