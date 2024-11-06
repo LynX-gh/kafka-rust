d90fe40c572089bd58a33e604483fad76a3bf109
lynx <76166621+LynX-gh@users.noreply.github.com>
2024-11-06 18:14:59 +0530

    Parse API Version, Major Refactor

--- src/main.rs
+++ src/main.rs
@@ -10,49 +10,54 @@ fn main() {
     // Uncomment this block to pass the first stage
     
     let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
-            Ok(mut _stream) => {
-                let mut response = vec![];
-                let mut len_buf = [0_u8; 4];
-                match _stream.read_exact(&mut len_buf) {
-                    Ok(_len) => {
-                        println!("Incoming Message Length : {:?}", len_buf);
-                        let msg_len = i32::from_be_bytes(len_buf);
-
-                        let mut msg_buf = vec![0_u8; msg_len as usize];
-                        match _stream.read_exact(&mut msg_buf) {
-                            Ok(_len) => {
-                                println!("Incoming Message Buffer : {:?}", msg_buf);
-
-                                let message_size:i32 = 20;
-
-                                response.put_i32(message_size);
-                                response.extend(&msg_buf[4..8]);
-                                response.put_i16(35_i16);
-                            }
-                            Err(e) => {
-                                println!("error: {}", e);
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        println!("error: {}", e);
-                    }
-                }
-
-                // println!("RESP : {:?}", response);
-
-                _stream.write(&response).unwrap();
-                let mut _remaining_buf = vec![];
-                _stream.read_to_end(&mut _remaining_buf).unwrap();
-
-                println!("accepted new connection");
+            Ok(mut stream) => {
+                handle_client(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_client<T: Read + Write>(stream: &mut T) {
+    let mut response = vec![];
+    let mut len_buf = [0_u8; 4];
+
+    if let Err(e) = stream.read_exact(&mut len_buf) {
+        println!("error: {}", e);
+        return;
+    }
+
+    println!("Incoming Message Length : {:?}", len_buf);
+    let msg_len = i32::from_be_bytes(len_buf);
+
+    let mut msg_buf = vec![0_u8; msg_len as usize];
+    if let Err(e) = stream.read_exact(&mut msg_buf) {
+        println!("error: {}", e);
+        return;
+    }
+
+    println!("Incoming Message Buffer : {:?}", msg_buf);
+
+    let message_size: i32 = 20;
+    response.put_i32(message_size);
+    response.extend(&msg_buf[4..8]);
+    response.put_i16(35_i16);
+
+    if let Err(e) = stream.write(&response) {
+        println!("error: {}", e);
+        return;
+    }
+
+    let mut remaining_buf = vec![];
+    if let Err(e) = stream.read_to_end(&mut remaining_buf) {
+        println!("error: {}", e);
+        return;
+    }
+
+    println!("accepted new connection");
 }
