30c18ba53e1b2fbb454245e70825c9e981ec85b6
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-01-05 16:01:45 +0530

    Ignored Test Scripts, Minor Changes to Config Structure

--- src/util.rs
+++ src/util.rs
@@ -5,25 +5,26 @@ use std::fs;
 pub struct ApiKeyDetail {
     pub key: i16,
     pub min_version: i16,
-    pub max_version: i16
+    pub max_version: i16,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct ApiKey {
-    pub supported_keys: Vec<i16>
+    pub supported_keys: Vec<i16>,
+    pub key: Vec<ApiKeyDetail>,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct ApiConfig {
     pub version: String,
-    pub endpoint: String
+    pub endpoint: String,
 }
 
 #[derive(Debug, Deserialize)]
 pub struct KafkaConfig {
     pub api: ApiConfig,
     pub api_key: ApiKey,
-    pub key: Vec<ApiKeyDetail>
+    // pub key: Vec<ApiKeyDetail>
 }
 
 impl KafkaConfig {
