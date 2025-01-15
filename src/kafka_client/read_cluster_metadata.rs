6ffdc3f7bddc73783fc23e3af9d3e68c84dc562d
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-01-16 01:07:28 +0530

    Read Cluster Record Batches

--- src/kafka_client/read_cluster_metadata.rs
+++ src/kafka_client/read_cluster_metadata.rs
@@ -3,15 +3,28 @@ use tokio::fs::File;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub async fn read_cluster_metadata() -> Result<(), Error>{
-    let contents = tokio::fs::read_to_string("/tmp/kraft-combined-logs/__cluster_metadata-0/00000000000000000000.log").await?;
-    println!("File string :{}", contents);
+    let mut file = File::open("tmp/kraft-combined-logs/__cluster_metadata-0/00000000000000000000.log").await?;
+    // let mut contents = vec![];
+    // file.read_to_end(&mut contents);
 
-    let mut file = File::open("/tmp/kraft-combined-logs/__cluster_metadata-0/00000000000000000000.log").await?;
+    let mut record_batch = Vec::new();
+    let mut offset_buf = [0_u8; 8];
+    let mut len_buf = [0_u8; 4];
 
-    let mut contents = vec![];
-    file.read_to_end(&mut contents).await?;
+    loop {
+        let data_offset = file.read(&mut offset_buf).await?;
+        let data_len = file.read(&mut len_buf).await?;
+        if data_offset == 0 || data_len == 0 {
+            break;
+        }
+        
+        let mut msg_buf = vec![0_u8; i32::from_be_bytes(len_buf) as usize];
+        file.read_exact(&mut msg_buf).await?;
+        record_batch.push(msg_buf);
 
-    println!("File Data - {:?}", contents);
-    println!("len = {}", contents.len());
+    }
+
+    println!("Record Batch Len - {}", record_batch.len());
+    println!("File Record Batch - {:?}", record_batch);
     Ok(())
 }
